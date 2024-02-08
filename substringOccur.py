aa7521687cf81088c94c16e358690fac02e20dc9
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-08 16:48:33 +0530

    [Nutan] Added: find number of occurance of given substring

---
+++ substringOccur.py
@@ -0,0 +1,10 @@
+string=input("Enter the string :")
+val= input("Enter the substring you want to count occurance for :")
+
+def counting(str1,sub):
+    str1= str1.lower()
+    print(str1.count(sub))
+
+counting(string,val)
+str1 = 'GOOGLEWALA.COM'
+print(str1[:4].lower() + str1[4:])
